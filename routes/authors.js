71d25073e418fe33277e7c211166251adf2e4b5c
Gladson Dutra <gladsonddutra@gmail.com>
2023-10-04 18:40:44 -0300

    Books routes Create/Index/New

--- routes/authors.js
+++ routes/authors.js
@@ -2,12 +2,12 @@ const express = require('express')
 const router = express.Router()
 const Author = require('../models/author')
 
+// All Authors Route
 router.get('/', async (req, res) => {
   let searchOptions = {}
   if (req.query.name != null && req.query.name !== '') {
     searchOptions.name = new RegExp(req.query.name, 'i')
   }
-
   try {
     const authors = await Author.find(searchOptions)
     res.render('authors/index', {
@@ -19,10 +19,12 @@ router.get('/', async (req, res) => {
   }
 })
 
+// New Author Route
 router.get('/new', (req, res) => {
   res.render('authors/new', { author: new Author() })
 })
 
+// Create Author Route
 router.post('/', async (req, res) => {
   const author = new Author({
     name: req.body.name
